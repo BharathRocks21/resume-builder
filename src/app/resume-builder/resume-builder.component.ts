2e62df2a2afdadc4fd8d25a20ad0b314f22da98e
BharathRocks21 <bbcc010151@gmail.com>
2025-05-26 22:49:26 +0530

    import module

--- src/app/resume-builder/resume-builder.component.ts
+++ src/app/resume-builder/resume-builder.component.ts
@@ -4,11 +4,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume-builder',
   standalone: true,
-  imports: [MatFormFieldModule, FormsModule, ReactiveFormsModule, MatInputModule, HttpClientModule],
+  imports: [MatFormFieldModule, FormsModule, ReactiveFormsModule, MatInputModule, HttpClientModule, CommonModule],
   templateUrl: './resume-builder.component.html',
   styleUrls: ['./resume-builder.component.css']
 })
